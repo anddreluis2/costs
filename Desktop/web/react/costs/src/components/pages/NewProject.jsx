797dc85855df525bb4742a80a0ce18d5efcc55b1
André Luis <anddreluis98@gmail.com>
2022-02-19 18:52:40 -0300

    Criação do form e pagina new project

--- Desktop/web/react/costs/src/components/pages/NewProject.jsx
+++ Desktop/web/react/costs/src/components/pages/NewProject.jsx
@@ -1,5 +1,14 @@
+import ProjectForm from '../Project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return ( 
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adcionar os serviços.</p>
+           <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProject
