79ebc7609978be32e8ee4c086fef0d436ec91634
André Luis <anddreluis98@gmail.com>
2022-02-20 15:33:36 -0300

    formulario bala

--- Desktop/web/react/costs/src/components/Project/ProjectForm.jsx
+++ Desktop/web/react/costs/src/components/Project/ProjectForm.jsx
@@ -1,20 +1,16 @@
-function ProjectForm(){
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import Submit from '../form/Submit'
+
+
+function ProjectForm({btnText}){
     return (
-        <form>
-            <div>
-                <input type="text" placeholder = "Insira o None do seu projeto:" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total:" />
-            </div>
-            <div>
-             <select name="category_id">
-                    <option disabled selected>Selecione a categoria</option>
-             </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto"/>
-            </div>
+        <form className={styles.ProjectForm}>
+            <Input type="text" text="Nome Projeto" name = "name" placeholder = "Insira o nome do projeto" />
+            <Input type="number" text="Orçamento" name = "budget" placeholder = "Insira o valor do orçamento" />
+            <Select name="category_id" text="Selecione a categoria"  />
+            <Submit text={btnText}/>
         </form>
     )
 }
